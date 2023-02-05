9850e19c362fee6a5b735585a7b68555ce014774
Nikita <nikitkavershinin2019@gmail.com>
2023-02-05 12:16:28 +0300

    search

--- src/components/ui/search/Search.tsx
+++ src/components/ui/search/Search.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import searchLight from '../../assets/images/searchLight.svg'
 import searchDark from '../../assets/images/searchDark.svg'
 import styles from './Search.module.scss'
 
-export default function App() {
+export default function Search({ isImage }: never) {
 	const [value, setValue] = React.useState('')
+	const handleSubmit = (e: any) => {
+		// Prevent the browser from reloading the page
+		e.preventDefault()
+
+		// Read the form data
+		const form = e.target
+		const formData = new FormData(form)
+
+		// You can pass formData as a fetch body directly:
+		fetch('/some-api', { method: form.method, body: formData })
+
+		// Or you can work with it as a plain object:
+		const formJson = Object.fromEntries(formData.entries())
+		console.log(formJson)
+	}
+
 	return (
 		<div className={styles.search}>
 			<form
@@ -15,13 +31,17 @@ export default function App() {
 			>
 				<input
 					value={value}
+					id='dfd'
 					onChange={e => {
 						setValue(e.target.value)
 					}}
+					placeholder='Search...'
 					type='text'
-					name='hash'
+					name='search'
 				/>
-				<button type='submit'>submit</button>
+				<button type='submit'>
+					<img src={isImage ? searchDark : searchLight} alt='search' />
+				</button>
 			</form>
 		</div>
 	)
