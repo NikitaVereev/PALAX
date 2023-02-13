7ff06111a40e10f643f9506e5a20d2eb69f3e027
Nikita <nikitkavershinin2019@gmail.com>
2023-02-13 22:53:30 +0300

    new select

---
+++ src/components/data/options.data.ts
@@ -0,0 +1,2 @@
+export const optionsData = ['Steel', 'Plastic', 'Tree']
+export const dimensionsData = ['Dimensions 1', 'Dimensions 3', 'Dimensions 5']
--- src/components/screens/home/product-description/ProductDescription.tsx
+++ src/components/screens/home/product-description/ProductDescription.tsx
@@ -1,45 +1,84 @@
 import { FC } from 'react'
 import styles from './ProductDescription.module.scss'
 import { useState } from 'react'
-import Select, { components } from 'react-select'
+import cn from 'classnames'
 import '../../../../styles/react-select.scss'
+import { dimensionsData, optionsData } from '../../../data/options.data'
+import { arrow } from '../../../assets/icons'
 
 const ProductDescription: FC = () => {
-	const DropdownIndicator = (props: any) => {
-		return (
-			<components.DropdownIndicator {...props}>
-				<svg
-					width='22'
-					height='22'
-					viewBox='0 0 22 22'
-					fill='none'
-					xmlns='http://www.w3.org/2000/svg'
-				>
-					<path d='M2.10498 12.5757L1.021 11.4917C0.562011 11.0327 0.562011 10.2905 1.021 9.83642L10.5083 0.344237C10.9673 -0.114747 11.7095 -0.114747 12.1636 0.344237L21.6558 9.83643C22.1147 10.2954 22.1147 11.0376 21.6558 11.4917L20.5718 12.5757C20.1079 13.0396 19.3511 13.0298 18.897 12.5562L13.2915 6.67236L13.2915 20.7056C13.2915 21.355 12.769 21.8774 12.1196 21.8774L10.5571 21.8774C9.90771 21.8774 9.38525 21.355 9.38525 20.7056L9.38525 6.67236L3.77978 12.5562C3.32568 13.0347 2.56885 13.0444 2.10498 12.5757Z' />
-				</svg>
-			</components.DropdownIndicator>
-		)
-	}
+	const [isThrow, setIsThrow] = useState('')
+	const [isDimensions, setIsDimensions] = useState('')
+	const [isActiveThrow, setActiveThrow] = useState(0)
+	const [isActiveDimensions, setIsActiveDimensions] = useState(0)
 
-	const options = [
-		{ value: 'steel', label: 'Steel' },
-		{ value: 'plastic', label: 'Plastic' },
-		{ value: 'tree', label: 'Tree' },
-	]
+	const [isShowThrow, setIsShowThrow] = useState(false)
+	const [isShowDimensions, setIsShowDimensions] = useState(false)
+
 	return (
 		<div className={styles.wrapper}>
-			<Select
-				components={{ DropdownIndicator }}
-				className={styles.select}
-				options={options}
-				placeholder={'Material'}
-			/>
-			<Select
-				className={styles.select}
-				components={{ DropdownIndicator }}
-				options={options}
-				placeholder={'Dimensions'}
-			/>
+			<div>
+				<button
+					className={cn(isShowThrow ? styles.active : styles.select)}
+					onClick={() => setIsShowThrow(!isShowThrow)}
+				>
+					{isThrow ? optionsData[isActiveThrow] : 'Throw'}
+					<svg
+						width='22'
+						height='22'
+						viewBox='0 0 22 22'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path d={arrow} />
+					</svg>
+				</button>
+				{isShowThrow &&
+					optionsData.map((option, idx) => (
+						<button
+							className={styles.select}
+							onClick={() => {
+								setIsThrow(`${option}`)
+								setActiveThrow(idx)
+								setIsShowThrow(false)
+							}}
+							key={idx}
+						>
+							{option}
+						</button>
+					))}
+			</div>
+			<div>
+				<button
+					className={cn(isShowDimensions ? styles.active : styles.select)}
+					onClick={() => setIsShowDimensions(!isShowDimensions)}
+				>
+					{isDimensions ? dimensionsData[isActiveDimensions] : 'Dimensions'}
+					<svg
+						width='22'
+						height='22'
+						viewBox='0 0 22 22'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path d={arrow} />
+					</svg>
+				</button>
+				{isShowDimensions &&
+					dimensionsData.map((option, idx) => (
+						<button
+							className={styles.select}
+							onClick={() => {
+								setIsDimensions(`${option}`)
+								setIsActiveDimensions(idx)
+								setIsShowDimensions(false)
+							}}
+							key={idx}
+						>
+							{option}
+						</button>
+					))}
+			</div>
 		</div>
 	)
 }
